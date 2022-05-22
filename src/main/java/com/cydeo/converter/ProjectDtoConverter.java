c7bc0c4dabc2e24d4570bebc5dc4f529ad67c394
Cemil <chrisw@cydeo.com>
2022-05-22 13:23:37 +0300

    MVC alumni batch1

--- src/main/java/com/cydeo/converter/ProjectDtoConverter.java
+++ src/main/java/com/cydeo/converter/ProjectDtoConverter.java
@@ -7,6 +7,7 @@ import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
 @Component
+//@ConfigurationPropertiesBinding   OPTIONAL
 public class ProjectDtoConverter implements Converter<String, ProjectDTO> {
 
     @Autowired
